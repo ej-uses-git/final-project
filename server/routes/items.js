22d90fb77712b3646b6d41638bbf764a085a577d
tehila1742 <tehila1742@gmail.com>
2022-10-18 19:49:16 +0300

    upload photos

--- server/routes/items.js
+++ server/routes/items.js
@@ -34,4 +34,37 @@ router.get("/:itemId/photos", async (req, res, next) => {
   }
 });
 
+router.post("/:itemId/uplodaphotos", async (req, res, next) => {
+  const { connect, query, end } = makeConnection();
+  let dirName;
+  try {
+    await connect();
+    dirName = await query(
+      `SELECT photos
+      FROM item
+      WHERE item_id = ${req.params.itemId}`
+    );
+    dirName = dirName[0]?.photos;
+    if (!result) return res.json([]);
+
+    for (let key in req.files) {
+      const file = req.files[key];
+      const newPath = path.join(
+        __dirname,
+        "../public/images/items/",
+        dirName,
+        file.name
+      );
+      const err = await util.promisify(file.mv)(newPath);
+      if (err) throw new Error("couldn't upload files");
+    }
+
+    await end();
+    res.json(true);
+  } catch (error) {
+    if (!error.fatal) await end();
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
 module.exports = router;
