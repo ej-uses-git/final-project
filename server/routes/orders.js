95391b66ed8a26c96234f1bbd9c301c4f5f5bff2
ej <103414353+ej-uses-git@users.noreply.github.com>
2022-10-18 15:03:27 +0300

    Server-Url

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const SERVER_URL = require('../utilities/server-url.json')
+// /api/orders
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+const makeConnection = require("../utilities/makeConnection");
+
+const SERVER_URL = require('../utilities/server-url.json')
+// /api/users
+
+// GET User Info
+router.get("/:userId/info", async (req, res, next) => {
+  const {connect, query, end} = makeConnection();
+  let result;
+  try {
+    await connect();
+    result = await query()
+  } catch (error) {
+    
+  }
 });
 
 module.exports = router;
