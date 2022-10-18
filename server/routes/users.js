f0f1bf7d36d43b23b2d533f5eaf494e91a970c02
ej <103414353+ej-uses-git@users.noreply.github.com>
2022-10-18 15:20:01 +0300

    temp

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,16 +2,39 @@ const express = require("express");
 const router = express.Router();
 
 const makeConnection = require("../utilities/makeConnection");
+const safelyEscape = require("../utilities/safelyEscape");
 
 // /api/users
 
 // GET User Info
 router.get("/:userId/info", async (req, res, next) => {
+  const { connect, query, end } = makeConnection();
+  let result;
+  try {
+    await connect();
+    result = await query(
+      `SELECT user_id, user_name, email
+      FROM user
+      WHERE user_id = ${req.params.userId}`
+    );
+    console.log("\n== result ==\n", result, "\n");
+    res.send(result[0]);
+  } catch (error) {
+    if (!error.fatal) await end();
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// GET Cart For User
+router.get("/:userId/cart/:cartId", async (req, res, next) => {
   const {connect, query, end} = makeConnection();
   let result;
   try {
     await connect();
-    result = await query(`${SERVER_URL}`)
+    result = await query(
+      ``
+    )
   } catch (error) {
     
   }
